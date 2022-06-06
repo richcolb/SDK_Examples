23c90db2641b61352dcbb4945e6b7c4fa1dde6ee
EC2 Default User <ec2-user@ip-172-31-47-188.eu-west-1.compute.internal>
2022-06-06 14:38:02 +0000

    updates

--- Comprehend/Sentiment.py
+++ Comprehend/Sentiment.py
@@ -1,10 +1,9 @@
 import boto3
 
 client = boto3.client('comprehend')
+print(type(client))
 
-sentiment = client.detect_sentiment(Text = "This product was rubbish - dont buy it!!!", LanguageCode = 'en') #API call for sentiment analysis
-sentRes = sentiment['Sentiment'] #Positive, Neutral, or Negative
-sentScore = sentiment['SentimentScore'] #Percentage of Positive, Neutral, and Negative
-print(sentRes)
-print(sentScore)
+sentiment = client.detect_sentiment(Text = "This product was OK but I had a few issues with it", LanguageCode = 'en') #API call for sentiment analysis
+
+print(sentiment["Sentiment"])
 
